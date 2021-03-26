9ed3614f79ad32e91d23fdfc68c6add68ced1bba
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2021-03-25 21:27:41 -0300

    Bootstrap do backend

---
+++ rentcars/backend/src/main/java/com/okellyson/rentcars/RentcarsApplication.java
@@ -0,0 +1,13 @@
+package com.okellyson.rentcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentcarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentcarsApplication.class, args);
+	}
+
+}
---
+++ rentcars/backend/src/test/java/com/okellyson/rentcars/RentcarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okellyson.rentcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentcarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
